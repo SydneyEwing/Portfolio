819ff4b7e5e0bf1205c6949d04a6c33fb2c5279b
Sydney <sydneyewing1@gmail.com>
2024-09-09 12:44:00 -0600

    Code
    Added the HTML, CSS, and JS

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+var modal = document.getElementById("mymodal");
+var btn = document.getElementById("mybtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    console.log(event)
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
